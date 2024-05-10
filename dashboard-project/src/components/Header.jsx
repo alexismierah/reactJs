2e0b5d2429a58f710ebfee0d313856e326d7f7f6
alexismierah <160734223+alexismierah@users.noreply.github.com>
2024-05-10 23:17:04 +0800

    dashboard-project

---
+++ dashboard-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white py-4">
+      {/* Your header content here */}
+      Header Component
+    </header>
+  );
+};
+
+export default Header;
